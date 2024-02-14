f36fe2528e506ce93f9db0b9e84c3ab2fb476bdc
Hasibullah Mahmood <hasibullahmahmood@gmail.com>
2024-02-14 22:04:29 +0300

    utilizing font and images are learned

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,15 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import clsx from 'clsx';
+import { inter } from './ui/fonts';
+import './ui/global.css';
+interface IProps {
+  readonly children: React.ReactNode;
+}
+
+export default function RootLayout(props: IProps) {
+  const { children } = props;
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={clsx([inter.className, 'antialiased'])}>{children}</body>
     </html>
   );
 }
